482234a9c5a00c045bf27f61c59920b68b38a74d
mozhutang <mozhutang@gmail.com>
2024-05-21 22:11:54 -0700

    Update hw5.js

--- homeworks/lecture4/hw5.js
+++ homeworks/lecture4/hw5.js
@@ -8,4 +8,25 @@
 
 const cloneDeepWithLoop = (obj) => {
     // Implement the function here
+    const seen = new WeakMap()
+
+    const clone = (item) => {
+        if (item === null || typeof item !== 'object'){
+            return item
+        }
+        if(seen.has(item)){
+            return seen.get(item)
+        }
+
+        const copy = Array.isArray(item) ? [] : {};
+        seen.set(item,copy)
+        for (const key in item){
+            if(item.hasOwnProperty(key)){
+                copy[key] = clone(item[key])
+            }
+        }
+
+        return copy
+    }
+    return clone(obj)
 }
