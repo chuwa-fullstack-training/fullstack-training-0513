fc1d79d7202f68731f8f2b825cd78534a011e4b6
Chenjie Wu <wucj.eric2015@outlook.com>
2024-05-22 16:55:59 -0700

    do more

--- homeworks/lecture4/hw2.js
+++ homeworks/lecture4/hw2.js
@@ -1,11 +1,23 @@
 // ONLY use map, filter, reduce to solve the following problems:
 
 // 1. Given an array of numbers, return an array of numbers that are doubled.
-
+function double (arr) {
+    return arr.map((v) => { return 2 * v; } );
+}
 // 2. Given an array of numbers, return an array of numbers that are even.
 
+function geteven (arr) {
+    return arr.filter((v) => { return v % 2 === 0; });
+}
 // 3. Reverse the string: "Hello World" -> "dlroW olleH"
-
+function reverse(str) {
+    let arr = str.split("");
+    let res = arr.reduce((prev, curr) => {
+        prev.unshift(curr);
+        return prev;
+    }, []);
+    return res.join("");
+}
 /**
  * 4. Flatten the array of arrays to a single array:
  * Example 1:
@@ -15,3 +27,22 @@
  * const arr = [[0, 1], [2, 3], [4, [5, 6]]];
  * Expected output: [0, 1, 2, 3, 4, 5, 6]
  */
+
+function flatten(arr) {
+    return arr.reduce((prev, curr) => {
+        if (typeof curr === "object") {
+            let flat = flatten(curr);
+            // console.log("element : " + flat);
+            prev = prev.concat(flat);
+            // console.log(prev);
+        } else {
+            prev.push(curr);
+        }
+        return prev;
+    }, []);
+}
+
+console.log(double([1, 2, 3]));
+console.log(geteven([1, 2, 3, 4]));
+console.log(reverse("cjwoo")); 
+console.log(flatten([[0, 1], [2, [3, [4, 5]], 3], [4, [5, 6]]]));
--- homeworks/lecture4/hw3.js
+++ homeworks/lecture4/hw3.js
@@ -8,3 +8,28 @@
  */
 
 // your code here
+
+// ES5
+function Singleton() {
+    var inst = this;
+    Singleton = function Singleton() {
+        return inst;
+    }
+    this.name = "XX";
+}
+// ES6
+// class Singleton {
+//     constructor() {
+//         if (Singleton._instance) {
+//             return Singleton._instance;
+//         }
+//         Singleton._instance = this;
+//         this.name = "xx";
+//     }
+// }
+
+const instance1 = new Singleton();
+const instance2 = new Singleton();
+console.log(instance1.name);
+console.log(instance2.name);
+console.log(instance1 === instance2); // Output: true
