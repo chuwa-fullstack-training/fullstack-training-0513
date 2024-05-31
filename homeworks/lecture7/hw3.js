3f3c8f409f746501e0a1e31c461aaec22d7ce934
mozhutang <mozhutang@gmail.com>
2024-05-30 23:25:06 -0700

    hw3

--- homeworks/lecture7/hw3.js
+++ homeworks/lecture7/hw3.js
@@ -9,3 +9,60 @@
  * 3. you need to figure out how to parse the query string in the home.html page
  * 4. after writing the html content, you need to write the query string in the html as well
  */
+
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const urlModule = require('url');
+
+const server = http.createServer((req, res) => {
+    const { url, method } = req;
+    if (method === 'GET') {
+        if (url === '/') {
+            res.end('this is the home page');
+        } else if (url === '/about') {
+            res.end('this is the about page');
+        } else if (url.startsWith('/home.html')) {
+            const queryObject = urlModule.parse(url, true).query;
+
+            fs.readFile(path.join(__dirname, 'home.html'), (err, html) => {
+                if (err) {
+                    res.end('error');
+                } else {
+                    res.writeHead(200, { 'Content-Type': 'text/html' });
+                    let responseHtml = html.toString();
+                    if (queryObject.title || queryObject.content) {
+                        responseHtml += `<p>Title: ${queryObject.title}</p><p>Content: ${queryObject.content}</p>`;
+                    }
+                    res.end(responseHtml);
+                }
+            });
+        } else {
+            res.end('this is the 404 page');
+        }
+    } else if (method === 'POST') {
+        if (url === '/create-post') {
+            let body = [];
+            req.on('data', chunk => {
+                body.push(chunk);
+            });
+            req.on('end', () => {
+                const parsedBody = Buffer.concat(body).toString();
+                const bodyData = new URLSearchParams(parsedBody);
+                const title = bodyData.get('title');
+                const content = bodyData.get('content');
+                res.statusCode = 302;
+                res.setHeader('Location', `/home.html?title=${encodeURIComponent(title)}&content=${encodeURIComponent(content)}`);
+                res.end();
+            });
+        } else {
+            res.end('this is the 404 page');
+        }
+    } else {
+        res.end('Unsupported method');
+    }
+});
+
+server.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
