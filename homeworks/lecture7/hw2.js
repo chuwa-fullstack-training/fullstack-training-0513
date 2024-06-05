7b632d183cf99d71712bf149a24a46869b8b7a12
Ping <pinghe2022@u.northwestern.edu>
2024-06-05 18:20:40 -0400

    lecture 7 homework

--- homeworks/lecture7/hw2.js
+++ homeworks/lecture7/hw2.js
@@ -19,3 +19,51 @@
  */
 
 // your code here
+const http = require('http');
+const PORT = 3000;
+const url = require('url');
+
+const sendJsonResponse = (res, statusCode, response) => {
+  res.writeHead(statusCode, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify(response));
+};
+
+const handleParseTime = (iso) => {
+  const date = new Date(iso);
+  return {
+    hour: date.getUTCHours(),
+    minute: date.getMinutes(),
+    second: date.getSeconds()
+  };
+};
+
+const handleUnixTime = (iso) => {
+  const date = new Date(iso);
+  return {
+    unixtime: date.getTime()
+  };
+};
+
+const server = http.createServer((req, res) => {
+  if (req.method === 'GET') {
+    const parsedUrl = url.parse(req.url, true);
+    const {pathname, query} = parsedUrl;
+    if (query.iso) {
+      if (pathname === '/api/parsetime') {
+        sendJsonResponse(res, 200, handleParseTime(query.iso));
+      } else if (pathname === '/api/unixtime') {
+        sendJsonResponse(res, 200, handleUnixTime(query.iso));
+      } else {
+        sendJsonResponse(res, 404, { error: 'Not Found' });
+      }
+    } else {
+      sendJsonResponse(res, 400, { error: 'Missing iso query parameter' });
+    }
+  } else {
+    sendJsonResponse(res, 404, {error: 'Unsupported method'});
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
