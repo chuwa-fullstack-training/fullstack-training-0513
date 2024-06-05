a512af1affe005b258b2345823675072a3f53521
huyiteng <yitengh2@illinois.edu>
2024-06-05 07:09:38 -0500

    complete hw7

--- homeworks/lecture7/hw1.js
+++ homeworks/lecture7/hw1.js
@@ -14,3 +14,24 @@
  */
 
 // your code here
+const fs = require('fs');
+const path = require('path'); 
+
+const dir = process.argv[2];
+const ext = process.argv[3];
+
+fs.readdir(dir, (err, files) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    files.forEach(file => {
+        if (path.extname(file) === `.${ext}`) {
+        console.log(file);
+        }
+    });
+    }
+);
+
+
+
--- homeworks/lecture7/hw3.js
+++ homeworks/lecture7/hw3.js
@@ -9,3 +9,61 @@
  * 3. you need to figure out how to parse the query string in the home.html page
  * 4. after writing the html content, you need to write the query string in the html as well
  */
+
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+  const { url, method } = req;
+  if (method === 'GET') {
+    if (url === '/') {
+      res.end('this is the home page');
+    } else if (url === '/about') {
+      res.end('this is the about page');
+    } else if (url.startsWith('/home.html')) {
+      fs.readFile(path.join(__dirname, 'home.html'), (err, html) => {
+        if (err) {
+          res.end('error');
+        } else {
+          res.writeHead(200, { 'Content-Type': 'text/html' });
+          res.write(html);
+          const query = url.split('?')[1]; // get the query string from the url
+          if (query) {
+            const queryData = query.split('&');
+            res.write('<ul>'); 
+            queryData.forEach(data => {
+                const [key, value] = data.split('=');
+                res.write(`<li>${key}: ${value}</li>`);
+            });
+            res.write('</ul>');
+          }
+          res.end();
+        }
+      });
+    } else {
+      res.end('this is the 404 page');
+    }
+  } else if (method === 'POST') {
+    if (url === '/create-post') {
+      let body = [];
+      req.on('data', chunk => { // collect the data from the form
+        body.push(chunk);
+      });
+      req.on('end', () => { // when the data is collected, parse it and send it back to the client
+        const parsedBody = Buffer.concat(body).toString(); // convert the data to string
+        res.statusCode = 302;
+        res.setHeader('Location', `/home.html?${parsedBody}`);
+        res.end(parsedBody);
+      });
+    } else {
+      res.end('this is the 404 page');
+    }
+  } else {
+    res.end('Unsupported method');
+  }
+});
+
+server.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
