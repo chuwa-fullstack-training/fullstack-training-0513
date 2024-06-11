d14bc8c9b57fa76eca42a1a2ccb640cf0e814ecb
huyiteng <yitengh2@illinois.edu>
2024-06-11 01:16:06 -0500

    complete hw12.1

---
+++ homeworks/lecture12/hw1.jsx
@@ -0,0 +1,29 @@
+{/* <h2>What is Frontend?</h2>
+<ul>
+  <li>
+    Frontend is the part of the website that users can see and interact with.
+  </li>
+  <li>Frontend is also called <strong>client-side</strong>.</li>
+  <li>Frontend is built with HTML, CSS, and JavaScript.</li>
+</ul> */}
+// turn this into a react component and display
+
+import React from 'react';
+
+const hw1 = () => {
+    return (
+        <div>
+        <h2>What is Frontend?</h2>
+        <ul>
+            <li>
+            Frontend is the part of the website that users can see and interact with.
+            </li>
+            <li>Frontend is also called <strong>client-side</strong>.</li>
+            <li>Frontend is built with HTML, CSS, and JavaScript.</li>
+        </ul>
+        </div>
+    );
+    };
+
+export default hw1;
+
