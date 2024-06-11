be2fe30aeb387ba767d0409b6c4149efeb07563d
huyiteng <yitengh2@illinois.edu>
2024-06-11 01:31:41 -0500

    Update hw1.jsx

--- homeworks/lecture12/hw1.jsx
+++ homeworks/lecture12/hw1.jsx
@@ -10,20 +10,22 @@
 
 import React from 'react';
 
-const hw1 = () => {
+class hw1 extends React.Component {
+  render() {
     return (
-        <div>
+      <div>
         <h2>What is Frontend?</h2>
         <ul>
-            <li>
+          <li>
             Frontend is the part of the website that users can see and interact with.
-            </li>
-            <li>Frontend is also called <strong>client-side</strong>.</li>
-            <li>Frontend is built with HTML, CSS, and JavaScript.</li>
+          </li>
+          <li>Frontend is also called <strong>client-side</strong>.</li>
+          <li>Frontend is built with HTML, CSS, and JavaScript.</li>
         </ul>
-        </div>
+      </div>
     );
-    };
+  }
+}
 
 export default hw1;
 
