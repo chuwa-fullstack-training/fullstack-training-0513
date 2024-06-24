bd0386abf971d63424b82e69d75d748069272a90
Jiayun Yan <Anyayan168@gmail.com>
2024-06-24 06:28:15 -0400

    hw13

--- homeworks/lecture13/hw/src/app/hw1/page.tsx
+++ homeworks/lecture13/hw/src/app/hw1/page.tsx
@@ -1,82 +1,106 @@
-"use client"
-import React, { useEffect, useState } from 'react';
+"use client";
+import React, { Component } from 'react';
 
-const Hw1 = () => {
-  const [inputValue, setInputValue] = useState("");
-  const [toDoList, setToDoList] = useState([]);
-  const [markAll, setMarkAll] = useState(false);
+class Hw1 extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: "",
+      toDoList: [],
+      markAll: false,
+    };
+  }
 
-  const count = toDoList.filter(todo => !todo.completed).length;
+  handleChange = (e) => {
+    this.setState({ inputValue: e.target.value });
+  };
 
-  const addToDo = () => {
+  addToDo = (event) => {
     if (event.key === 'Enter') {
-      setToDoList([...toDoList, { text: inputValue, completed: false }]);
-      setInputValue("");
+      this.setState((prevState) => ({
+        toDoList: [...prevState.toDoList, { text: prevState.inputValue, completed: false }],
+        inputValue: "",
+      }));
     }
   };
 
-  const handleCheck = index => {
-    const UpdatedList = [...toDoList];
-    UpdatedList[index].completed = !UpdatedList[index].completed;
-    setToDoList(UpdatedList);
+  handleCheck = (index) => {
+    this.setState((prevState) => {
+      const updatedList = prevState.toDoList.map((todo, i) => {
+        if (i === index) {
+          return { ...todo, completed: !todo.completed };
+        }
+        return todo;
+      });
+      return { toDoList: updatedList };
+    });
   };
 
-  const handleMarkAll = () => {
-    const newTodos = toDoList.map(todo => ({ ...todo, completed: !markAll }));
-    setToDoList(newTodos);
-    setMarkAll(!markAll);
+  handleMarkAll = () => {
+    this.setState((prevState) => {
+      const newTodos = prevState.toDoList.map((todo) => ({ ...todo, completed: !prevState.markAll }));
+      return { toDoList: newTodos, markAll: !prevState.markAll };
+    });
   };
 
-  const handleClear = () => {
-    const clearTodos = toDoList.filter(todo => !todo.completed);
-    setToDoList(clearTodos);
+  handleClear = () => {
+    this.setState((prevState) => {
+      const clearTodos = prevState.toDoList.filter((todo) => !todo.completed);
+      return { toDoList: clearTodos };
+    });
   };
 
-  return(
-    <div className='mx-auto w-4/5'>
-      <p className="text-3xl font-bold w-full">Todos - ReactJs</p>
-      <label className="block mt-4">
-        <input 
-          type="text"
-          className="p-2 border border-gray-200 rounded w-full focus:outline-blue-300"
-          placeholder='Type a todo and hit Enter' 
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
-          onKeyDown = {addToDo}
-        />
-      </label>
+  render() {
+    const { inputValue, toDoList, markAll } = this.state;
+    const count = toDoList.filter((todo) => !todo.completed).length;
 
-      <div className='grid grid-cols-2 justify-between mt-4'>
-        <p className='text-lg grid-span-1 py-2'>{count} remaining</p>
-        <button 
-          className='ms-auto grid-span-1 text-lg font-bold border border-black hover:bg-gray-300 hover:text-white hover:border-gray-500 rounded py-2 px-4 text-gray-400 w-fit'
-          onClick={handleClear}
-        >
-          Clear Completed Todos</button>
+    return(
+      <div className='mx-auto w-4/5'>
+        <p className="text-3xl font-bold w-full">Todos - ReactJs</p>
+        <label className="block mt-4">
+          <input 
+            type="text"
+            className="p-2 border border-gray-200 rounded w-full focus:outline-blue-300"
+            placeholder='Type a todo and hit Enter' 
+            value={inputValue}
+            onChange={this.handleChange}
+            onKeyDown={this.addToDo}
+          />
+        </label>
+  
+        <div className='grid grid-cols-2 justify-between mt-4'>
+          <p className='text-lg grid-span-1 py-2'>{count} remaining</p>
+          <button 
+            className='ms-auto grid-span-1 text-lg font-bold border border-black hover:bg-gray-300 hover:text-white hover:border-gray-500 rounded py-2 px-4 text-gray-400 w-fit'
+            onClick={this.handleClear}
+          >
+            Clear Completed Todos
+          </button>
+        </div>
+        
+        <label className="block mt-4 group text-lg">
+          <input 
+            type="checkbox"
+            className="mr-2"
+            checked={markAll}
+            onChange={this.handleMarkAll}
+          />
+          Mark All Done
+        </label>
+  
+        <ul className='mt-4'>
+          {toDoList.map((todo, index) => (
+            <li key={index} className='mt-1 border border-gray-200 p-2 rounded'>
+              <label className='group block text-lg'>
+                <input type="checkbox" checked={todo.completed} onClick={() => this.handleCheck(index)} className='mr-2'/>
+                {todo.text}
+              </label>
+            </li>
+          ))}
+        </ul>
       </div>
-      
-      <label className="block mt-4 group text-lg">
-        <input 
-          type="checkbox"
-          className="mr-2"
-          checked={markAll}
-          onChange={handleMarkAll}
-        />
-        Mark All Done
-      </label>
-
-      <ul className='mt-4'>
-        {toDoList.map((todo, index) => (
-          <li key={index} className='mt-1 border border-gray-200 p-2 rounded'>
-            <label className='group block text-lg'>
-              <input type="checkbox" checked={todo.completed} onClick={() => handleCheck(index)} className='mr-2'/>
-              {todo.text}
-            </label>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+    );
+  }
 }
 
 export default Hw1;
